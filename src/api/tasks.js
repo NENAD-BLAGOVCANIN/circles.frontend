360ef9e3a2b6dd4494981f8f7cff933b60edd580
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-03-28 00:56:22 +0100

    tasks

---
+++ src/api/tasks.js
@@ -0,0 +1,83 @@
+import { apiUrl } from './config';
+
+const getTasks = async () => {
+
+    try {
+
+        const token = localStorage.getItem('accessToken'); 
+
+        const response = await fetch(apiUrl + '/tasks', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+        });
+
+        const responseData = await response.json();
+
+        if (response.ok) {
+            return responseData;
+        } else {
+            throw new Error(responseData.errors);
+        }
+    } catch (error) {
+        throw new Error(error);
+    }
+
+}
+
+const saveTask = async (contact) => {
+
+    try {
+
+        const token = localStorage.getItem('accessToken'); 
+
+        const response = await fetch(apiUrl + '/contacts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+            body: JSON.stringify(contact)
+        });
+
+        const responseData = await response.json();
+
+        return responseData;
+
+    } catch (error) {
+        return error;
+    }
+
+}
+
+
+const deleteTask = async (contact_id) => {
+
+    try {
+
+        const token = localStorage.getItem('accessToken'); 
+
+        const response = await fetch(apiUrl + '/contacts/' + contact_id, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+        });
+
+        const responseData = await response.json();
+
+        return responseData;
+
+    } catch (error) {
+        return error;
+    }
+
+}
+
+
+export { getTasks, saveTask, deleteTask };
--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Sidebar from '../components/Sidebar'
 import Header from '../components/Header'
+import { getTasks } from '../api/tasks'
 
 function Tasks() {
+
+
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        const fetchTasks = async () => {
+            try {
+                const fetchedTasks = await getTasks();
+                setTasks(fetchedTasks);
+            } catch (error) {
+                console.error('Error fetching :', error);
+            }
+        };
+
+        fetchTasks();
+    }, []);
+
     return (
         <div className='main-content-wrapper'>
 
@@ -15,35 +33,44 @@ function Tasks() {
                 <div className='main-container'>
 
                     <div className="row">
-                        <div className="col-md-4 p-3">
-                            <div className="card">
-                                <div className=''>
-                                    <h4 className='pe-2'>Task Name</h4>
-                                    <span className='text-muted'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet culpa vitae porro soluta dolores voluptas nihil esse temporibus, 
-                                        non est, minus doloribus exercitationem totam laboriosam pariatur magnam cumque atque ea!</span>
-                                </div>
-
+                        <div className="col-4 p-3">
+                            <div className='bg-dark rounded text-white text-center mb-3 py-2'>
+                                TODO
                             </div>
+                            {tasks.filter(task => task.status === 'todo').map(task => (
+                                <div className="card mb-3">
+                                    <div className=''>
+                                        <h4 className='pe-2'>{task.subject}</h4>
+                                        <span className='text-muted'>{task.description}</span>
+                                    </div>
+                                </div>
+                            ))}
                         </div>
-                        <div className="col-md-4 p-3">
-                            <div className="card">
-                                <div className=''>
-                                    <h4 className='pe-2'>Task Name</h4>
-                                    <span className='text-muted'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet culpa vitae porro soluta dolores voluptas nihil esse temporibus, 
-                                        non est, minus doloribus exercitationem totam laboriosam pariatur magnam cumque atque ea!</span>
-                                </div>
-
+                        <div className="col-4 p-3">
+                            <div className='bg-dark rounded text-white text-center mb-3 py-2'>
+                                IN PROGRESS
                             </div>
+                            {tasks.filter(task => task.status === 'in_progress').map(task => (
+                                <div className="card mb-3">
+                                    <div className=''>
+                                        <h4 className='pe-2'>{task.subject}</h4>
+                                        <span className='text-muted'>{task.description}</span>
+                                    </div>
+                                </div>
+                            ))}
                         </div>
-                        <div className="col-md-4 p-3">
-                            <div className="card">
-                                <div className=''>
-                                    <h4 className='pe-2'>Task Name</h4>
-                                    <span className='text-muted'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet culpa vitae porro soluta dolores voluptas nihil esse temporibus, 
-                                        non est, minus doloribus exercitationem totam laboriosam pariatur magnam cumque atque ea!</span>
-                                </div>
-
+                        <div className="col-4 p-3">
+                            <div className='bg-dark rounded text-white text-center mb-3 py-2'>
+                                DONE
                             </div>
+                            {tasks.filter(task => task.status === 'done').map(task => (
+                                <div className="card mb-3">
+                                    <div className=''>
+                                        <h4 className='pe-2'>{task.subject}</h4>
+                                        <span className='text-muted'>{task.description}</span>
+                                    </div>
+                                </div>
+                            ))}
                         </div>
                     </div>
 
