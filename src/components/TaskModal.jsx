900818385b18419cc791fd57edab701fbd46f48b
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-03-28 04:59:10 +0100

    updates

--- src/components/TaskModal.jsx
+++ src/components/TaskModal.jsx
@@ -1,14 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { updateTask } from '../api/tasks';
 
 function TaskModal({ showTasksModal, setShowTasksModal, selectedTask, setSelectedTask, tasks, setTasks }) {
+    const [editedTitle, setEditedTitle] = useState(selectedTask.subject);
+    const [editedDescription, setEditedDescription] = useState(selectedTask.description);
+
+    useEffect(() => {
+        setEditedTitle(selectedTask.subject);
+        setEditedDescription(selectedTask.description);
+    }, [selectedTask]);
 
     const handleCloseTaskModal = () => {
         setShowTasksModal(false);
     };
 
+    const handleTitleChange = (event) => {
+        setEditedTitle(event.target.textContent);
+    };
+
+    const handleDescriptionChange = (event) => {
+        setEditedDescription(event.target.textContent);
+    };
+
+    const handleBlur = async () => {
+        const updatedTask = {
+            ...selectedTask,
+            subject: editedTitle,
+            description: editedDescription
+        };
+
+        const response = await updateTask(updatedTask);
+        
+        setSelectedTask(updatedTask);
+
+        console.log(updateTask);
+
+        const updatedTasks = tasks.map(task => {
+            if (task.id === updatedTask.id) {
+                return updatedTask;
+            }
+            return task;
+        });
+        setTasks(updatedTasks);
+
+    };
+
     const changeTaskStatus = async (status) => {
-        const taskIndex = tasks.findIndex(task => task.id === selectedTask.id);
+        const taskIndex = tasks.findIndex((task) => task.id === selectedTask.id);
         if (taskIndex !== -1) {
             const updatedTasks = [...tasks];
             updatedTasks[taskIndex] = {
@@ -23,16 +61,12 @@ function TaskModal({ showTasksModal, setShowTasksModal, selectedTask, setSelecte
             status: status
         };
 
-        console.log(updatedTask);
-
         const response = await updateTask(updatedTask);
 
-        if (response.ok){
+        if (response.ok) {
             setSelectedTask(updatedTask);
         }
-
     };
-
 
     return (
         <>
@@ -41,7 +75,9 @@ function TaskModal({ showTasksModal, setShowTasksModal, selectedTask, setSelecte
                     <div className="modal-content py-3 px-4 border-0 shadow-lg" style={{ maxHeight: 800, overflow: 'auto' }}>
                         <div className="modal-header pb-0 border-0 d-flex align-items-center">
                             <div>
-                                <h4 className="modal-title bold m-0">{selectedTask.subject}</h4>
+                                <h4 className="modal-title bold m-0" onBlur={handleBlur}>
+                                    <span contentEditable suppressContentEditableWarning onBlur={handleBlur} onInput={handleTitleChange}>{editedTitle}</span>
+                                </h4>
                                 <span className='small text-muted'>
                                     In list <u>{selectedTask.status}</u>
                                 </span>
@@ -51,16 +87,11 @@ function TaskModal({ showTasksModal, setShowTasksModal, selectedTask, setSelecte
                             </span>
                         </div>
                         <div className='modal-body py-5'>
-
                             <h5>Description</h5>
-
-                            <p className='text-muted'>
-                                {selectedTask.description}
+                            <p className='text-muted' onBlur={handleBlur}>
+                                <span contentEditable suppressContentEditableWarning onBlur={handleBlur} onInput={handleDescriptionChange}>{editedDescription}</span>
                             </p>
-
                             <h5 className='mt-5'>Assigned to</h5>
-
-
                         </div>
                         <div className='modal-footer border-0'>
                             <div className='px-1'>
@@ -77,7 +108,7 @@ function TaskModal({ showTasksModal, setShowTasksModal, selectedTask, setSelecte
                 </div>
             </div>
         </>
-    )
+    );
 }
 
-export default TaskModal
+export default TaskModal;
